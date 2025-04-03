c6e1cd6144c10be5d15abad35a0d937ed20350a3
Bryan Bell <bjbsga@gmail.com>
2025-04-03 14:54:14 -0700

    rm maintenance scripts; saved to hidden dir

--- bin/addDynamics.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in *.krn
-do
-  pfDynam $i > dynam_$i
-done
--- bin/sysDec.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in */*.krn
-do
-  sed -i '' 's/!!!system.*/!!!system-decoration: {(*)}/' $i
-done
